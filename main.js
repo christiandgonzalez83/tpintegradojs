7ee39719173f6f440301f4b0c3470819911e4873
Christian Gonzalez <christiandgonzalez83@gmail.com>
2023-06-04 23:29:54 -0300

    Trabajo integrador JS

---
+++ main.js
@@ -0,0 +1,44 @@
+// const resultado = document.querySelector('#select');
+// const btnSumar = document.querySelector('.boton-sumar');
+// const btnRestar = document.querySelector('.boton-restar');
+// const botonBorrar = document.querySelector('.btnBorrar');
+// // console.log(btnSumar);
+// const nombre= document.querySelector('form');
+// const nombreValue = nombre.ariaValueText;
+//  btnResumen.addEventListener("click",()=>{    
+//     console.log(nombreValue);
+// //     parrafo.innerHTML = contador;
+//  })
+// document.querySelector('form')
+//     addEventListener('submit', e => {
+//         e.preventDefault()
+//         const datos = Object.fromEntries(
+//             new FormData(e.target)
+//         )
+//         console.log(datos);
+//     })
+
+// let vtotal = 200;
+
+// function select{
+//     if (document.select == opcion1 ) {
+        
+//     } else {
+        
+//     } document.getElementById("select")
+// }
+
+// btnSumar.addEventListener("click",()=>{    
+//     contador++;
+//     parrafo.innerHTML = contador;
+// })
+
+// btnRestar.addEventListener("click",()=>{    
+//     contador--;
+//     parrafo.innerHTML = contador;
+// })
+
+// btnReset.addEventListener("click",()=>{
+ //    vtotal = 0;
+//     resultado.innerHTML = vtotal;
+// })
